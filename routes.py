5ac3fb53c5d3aae3dda0adac93c8fa4a3270ccbc
oleg-potichnyi <potichnyi.oleg@gmail.com>
2024-05-28 16:04:02 +0300

    Add login and registration forms

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db
 from models import User, Ticket, Group
+from forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
@@ -14,11 +16,37 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # Handle login logic here
-    return render_template("login.html", title="Sign In")
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not check_password_hash(
+            user.password_hash, form.password.data
+        ):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
 
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.password_hash = generate_password_hash(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
